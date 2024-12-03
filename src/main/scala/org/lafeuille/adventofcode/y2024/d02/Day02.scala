ed73d89c032982a1ac444f8cc3674f52edd6f1fe
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2024-12-03 22:49:24 +0100

    WIP

--- src/main/scala/org/lafeuille/adventofcode/y2024/d02/Day02.scala
+++ src/main/scala/org/lafeuille/adventofcode/y2024/d02/Day02.scala
@@ -1,7 +1,5 @@
 package org.lafeuille.adventofcode.y2024.d02
 
-import org.lafeuille.adventofcode.y2024.d01.Day01.{getClass, myLists}
-
 import scala.annotation.tailrec
 import scala.io.Source
 
@@ -15,48 +13,24 @@ object Day02 {
         .map(_.toInt)
         .toList)
       .toList
-
-  def myReports: List[Report] =
-    myList.map(Report(_))
 }
-
-case class Report(levels: List[Int])
 
 object Day02Part1 extends App {
 
-  @tailrec
-  private def increasingRule(isAscending: Boolean, level: Int, levels: List[Int]): Boolean = levels match {
-    case Nil => true
-    case head :: tail if isAscending && level < head => increasingRule(isAscending, head, tail)
-    case head :: tail if !isAscending && level > head => increasingRule(isAscending, head, tail)
-    case _ => false
-  }
+  def isSafe(levels: List[Int]): Boolean = {
+    var isAscending = true
+    for (i <- levels.indices) {
 
-  def increasingRule(levels: List[Int]): Boolean = levels match {
-    case a :: b :: tail => increasingRule(a < b, b, tail)
-    case _ => true
+    }
+    false
   }
-
-  @tailrec
-  private def consecutiveRule(level: Int, levels: List[Int]): Boolean = levels match {
-    case Nil => true
-    case head :: tail => math.abs(level - head) > 0 && math.abs(level - head) < 4 && consecutiveRule(head, tail)
-  }
-
-  def consecutiveRule(levels: List[Int]): Boolean = levels match {
-    case Nil => true
-    case head :: tail => consecutiveRule(head, tail)
-  }
-
-  def isSafe(levels: List[Int]): Boolean =
-    increasingRule(levels) && consecutiveRule(levels)
 
   def result(reports: List[List[Int]]): Int =
     reports.count(isSafe)
 
+  println(Day02.myList.indices)
   println(result(Day02.myList))
 }
 
 object Day02Part2 extends App {
-
 }
