8f08f9abc7d12faf7228db786610190985cf7ab1
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2024-12-06 16:00:19 +0100

    Day06 Part 1

---
+++ src/main/scala/org/lafeuille/adventofcode/y2024/d06/Day06.scala
@@ -0,0 +1,19 @@
+package org.lafeuille.adventofcode.y2024.d06
+
+import org.lafeuille.adventofcode.y2024.d05.Day05.getClass
+
+import scala.io.Source
+
+object Day06 {
+  private def myInput: Source =
+    Source.fromURL(getClass.getResource("input06.txt"))
+
+}
+
+object Day06Part1 {
+
+}
+
+object Day06Part2 {
+
+}
